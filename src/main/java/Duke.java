e43ceeaa144fbfa8f99e7273e60a9ac38e230a05
Kyrixn <keyrixn@gmail.com>
2022-10-22 19:20:41 +0800

    some syntax modification

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,7 +2,7 @@ public class Duke {
     public static void main(String[] args) {
         String line = "---------------------------------------------------------";
         System.out.println(line);
-        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+        System.out.println("Hello! I'm Duke"+System.lineSeparator()+"What can I do for you?");
         System.out.println(line);
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println(line);
