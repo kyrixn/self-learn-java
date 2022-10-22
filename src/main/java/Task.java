c51f6a791e166b8ebaeb1fba185d2b31ff831f9d
Kyrixn <keyrixn@gmail.com>
2022-10-22 19:56:32 +0800

    level-2

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -7,6 +7,10 @@ public class Task {
         this.isDone = false;
     }
 
+    public String getTaskDiscription() {
+        return this.taskDiscription;
+    }
+
     public String getTaskStatus() {
         return (this.isDone ? "X" : " ");
     }
